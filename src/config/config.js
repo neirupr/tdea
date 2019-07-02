f63d9246fc2c20721ac64230357f0f6517dc3e1d
Neiro Torres <neiro.torres@keyrus.com>
2019-07-02 16:08:53 -0500

    mongoDB atlas ready

--- src/config/config.js
+++ src/config/config.js
@@ -1,1 +1,3 @@
 process.env.PORT = process.env.PORT || 3000
+process.env.URLDB = 'mongodb://localhost:27017/neiro'
+//process.env.URLDB = 'mongodb+srv://{{DBNAME}}:{{DBPASSWORD}}@neirokeyrus-fsp0w.mongodb.net/{{DBNAME}}?retryWrites=true&w=majority'
