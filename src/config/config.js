f41649bc9a5b03a2bd62e82836718086ee45ef11
Neiro Torres <neiro.torres@keyrus.com>
2019-07-02 16:26:09 -0500

    Fixed environment

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,4 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.URLDB = 'mongodb://localhost:27017/neiro'
 //process.env.URLDB = 'mongodb+srv://{{DBNAME}}:{{DBPASSWORD}}@neirokeyrus-fsp0w.mongodb.net/{{DBNAME}}?retryWrites=true&w=majority'
+//process.env.URLDB = 'mongodb+srv://neiro:Keyrus.123@neirokeyrus-fsp0w.mongodb.net/neiro?retryWrites=true&w=majority'
