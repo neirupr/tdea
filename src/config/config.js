f7908c88a8ac18762b42c390b4cba6d4cbd65a66
neirupr <neiroandres@yahoo.com.co>
2019-07-28 20:56:10 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
 process.env.PORT = process.env.PORT || 3000
 process.env.URLDB = process.env.URLDB || 'mongodb://localhost:27017/neiro'
 process.env.SENDGRID_API_KEY = process.env.SENDGRID_API_KEY || 'nothing'
-//process.env.URLDB = 'mongodb+srv://neiro:Keyrus.123@neirokeyrus-fsp0w.mongodb.net/neiro?retryWrites=true&w=majority'
